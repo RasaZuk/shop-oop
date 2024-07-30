0ce9e6a24046b1b063f36b0d1e04f7784e6d0de8
Rasa <rasa.zukauskaite@gmail.com>
2024-07-30 10:02:03 +0300

    shop oop lesson update

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -1,52 +1,87 @@
+import { PageContact } from "./PageContact.js";
+import { PageHome } from "./PageHome.js";
+import { PageServices } from "./PageServices.js";
+import { PageTeam } from "./PageTeam.js";
+
 export class Layout {
     constructor() {
+        this.pagesData = [
+            {
+                text: 'Home',
+                content: PageHome,
+                background: 'pink',
+                title: 'Home',
+            },
+            {
+                text: 'Team',
+                content: PageTeam,
+                background: 'grey',
+                title: 'Our team'
+            },
+            {
+                text: 'Services',
+                content: PageServices,
+                background: 'white',
+                title: 'Our services'
+            },
+            {
+                text: 'Contact us',
+                content: PageContact,
+                background: 'aquamarine',
+                title: 'Contact us',
+            },
+        ];
         this.render();
     }
 
-
     header() {
-        const HTML = `
-        <header class="container">
-            <div class="row">
-                <div class="col-12">
-                    <img src="#" alt="Logo">
-                    <nav style="display:inline-block;" class="hidden visible-sm">
-                        <a href="./">Home</a>
-                        <a href="./services">Services</a>
-                        <a href="./team">Team</a>
-                        <a href="./contact-us">Contact us</a>
-                    </nav> 
+        let navHTML = '';
+
+        for (const link of this.pagesData) {
+            navHTML += `<button class="link">${link.text}</button>`;
+        }
+
+        return `
+            <header class="container main-header">
+                <div class="row">
+                    <div class="col-12 main-header-content">
+                        <img class="logo" src="./img/logo.png" alt="Logo">
+                        <nav class="hidden visible-sm-flex main-nav">
+                            ${navHTML}
+                        </nav>
+                    </div>
                 </div>
-            </div>    
-        </header>`;
-        return HTML;
+            </header>`;
+    }
+
+    headerEvents() {
+        const buttonsDOM = document.querySelectorAll('.main-header-content button');
+        const mainDOM = document.querySelector('main.container');
+        const titleDOM = document.querySelector('head title');
+
+        for (let i = 0; i < buttonsDOM.length; i++) {
+            const buttonDOM = buttonsDOM[i];
+            buttonDOM.addEventListener('click', () => {
+                const pageClass = this.pagesData[i].content;
+                mainDOM.innerHTML = (new pageClass()).render();
+                document.body.style.backgroundColor = this.pagesData[i].background;
+                titleDOM.textContent = this.pagesData[i].title;
+            });
+        }
     }
 
     main() {
+        const pageObject = new PageContact();
         const HTML = `
-        <main class="container">
-            <section class="row">
-            <h1 class="col-12 main-title">Pirmas posukis</h1>
-            </section>
-               
-            <section class="row">
-                <div class="col-10 col-sm-8">TarpasA</div>
-                <div class="col-1 m-1 m-sm-3">TarpasB</div>
-            </section>
-            <section class="row">
-                <div class="col-12 col-sm-6 col-lg-4 col-xl-3 col-xxl-12">Pirmas posukis</div>
-                <div class="col-12 col-sm-6 col-lg-4 col-xl-3">Pirmas posukis</div>
-                <div class="col-6 col-lg-4 col-xl-3">Antras posukis</div>
-                <div class="col-6 col-lg-4 col-xl-3">Antras posukis</div>
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-1">Trecias posukis</div>
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2">Trecias posukis.</div>
-            </section>
-        </main>`;
+            <main class="container">
+                ${pageObject.render()}
+            </main>`;
+
         return HTML;
     }
 
     footer() {
-        const HTML = `<footer class="container">&copy; Copyright 2024</footer>`;
+        const HTML = '<footer class="container">&copy; Copyright 2024</footer>';
         return HTML;
     }
 
@@ -55,5 +90,7 @@ export class Layout {
         const HTML = this.header() + this.main() + this.footer();
 
         DOM.insertAdjacentHTML('beforeend', HTML);
+
+        this.headerEvents();
     }
 }
